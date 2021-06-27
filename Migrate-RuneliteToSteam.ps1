75cb2519f8226cb0086c1cb7432f25c6847078ce
ewisted <ew@atuw.org>
2021-06-27 01:03:15 -0500

    Updated script and readme

--- Migrate-RuneliteToSteam.ps1
+++ Migrate-RuneliteToSteam.ps1
@@ -1,10 +1,14 @@
-﻿param(
+﻿[CmdletBinding(DefaultParameterSetName="Migrate")]
+param(
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     [string]$RuneLiteInstallPath,
     [Parameter(Mandatory=$true)]
     [ValidateNotNullOrEmpty()]
     [string]$SteamClientInstallPath,
+    [Parameter(ParameterSetName="Schedule")]
+    [ValidateSet("Startup", "Logon")]
+    [string]$Schedule,
     [Parameter()]
     [switch]$Revert
 )
@@ -48,55 +52,83 @@ if (!(Test-Path $runeliteClientPath) -or
     return
 }
 
-if ($Revert) {
-    Remove-Item -Path  $steamClientPath -Force
-    Remove-Item -Path $steamJrePath -Recurse -Force
-    Remove-Item -Path $steamJarPath -Force
-    Remove-Item -Path $steamConfigPath -Force
-    Remove-Item -Path $migrationTrackerPath -Force
-    Rename-Item -Path $steamClientBackupPath -NewName $steamClientPath -Force
-    Write-Output "Reverted back to OSRS Steam client."
-    return
-}
-
-# Current client hashes
-$currentRuneliteClientHash = (Get-FileHash -Algorithm MD5 -Path $runeliteClientPath).Hash
-$currentSteamClientHash = (Get-FileHash -Algorithm MD5 -Path $steamClientPath).Hash
-
-if (Test-Path $migrationTrackerPath) 
+switch ($PSCmdlet.ParameterSetName)
 {
-    $migrationTracker = Get-Content -Path $migrationTrackerPath -Raw | ConvertFrom-Json
-
-    # Check if client has been updated by Steam or if RuneLite has updated since last run. If neither case is true, take no action.
-    if ($currentSteamClientHash -ne $migrationTracker.lastRuneliteClientHash)
+    "Migrate"
     {
-        Write-Output "Steam client update detected. Copying RuneLite files..."
-        if (Test-Path $steamClientBackupPath) 
+        if ($Revert)
         {
-            Remove-Item $steamClientBackupPath
+            Remove-Item -Path  $steamClientPath -Force
+            Remove-Item -Path $steamJrePath -Recurse -Force
+            Remove-Item -Path $steamJarPath -Force
+            Remove-Item -Path $steamConfigPath -Force
+            Remove-Item -Path $migrationTrackerPath -Force
+            Rename-Item -Path $steamClientBackupPath -NewName $steamClientPath -Force
+            Write-Output "Reverted back to OSRS Steam client."
+            return
         }
-        Rename-Item -Path $steamClientPath -NewName $steamClientBackupPath -Force
-        Copy-Files
-    }
-    elseif ($currentRuneliteClientHash -ne $migrationTracker.lastRuneliteClientHash) {
-        Write-Output "RuneLite update detected. Copying new RuneLite files..."
-        $migrationTracker.lastRuneliteClientHash = $currentRuneliteClientHash
-        Copy-Files
-    }
-    else
-    {
-        Write-Output "Neither RuneLite or the Steam client have been updated since last run. Cancelling migraion."
-        return
-    }
-}
-else 
-{
-    $migrationTracker = New-Object -TypeName MigrationTracker
-    $migrationTracker.lastRuneliteClientHash = $currentRuneliteClientHash
-    Rename-Item -Path $steamClientPath -NewName $steamClientBackupPath -Force
-    Copy-Files
-}
 
-$migrationTracker.lastMigrated = (Get-Date).ToString()
-$migrationTracker | ConvertTo-Json | Out-File -FilePath $migrationTrackerPath
-Write-Output "Migration completed successfully."
+        # Current client hashes
+        $currentRuneliteClientHash = (Get-FileHash -Algorithm MD5 -Path $runeliteClientPath).Hash
+        $currentSteamClientHash = (Get-FileHash -Algorithm MD5 -Path $steamClientPath).Hash
+
+        if (Test-Path $migrationTrackerPath) 
+        {
+            $migrationTracker = Get-Content -Path $migrationTrackerPath -Raw | ConvertFrom-Json
+
+            # Check if client has been updated by Steam or if RuneLite has updated since last run. If neither case is true, take no action.
+            if ($currentSteamClientHash -ne $migrationTracker.lastRuneliteClientHash)
+            {
+                Write-Output "Steam client update detected. Copying RuneLite files..."
+                if (Test-Path $steamClientBackupPath) 
+                {
+                    Remove-Item $steamClientBackupPath
+                }
+                Rename-Item -Path $steamClientPath -NewName $steamClientBackupPath -Force
+                Copy-Files
+            }
+            elseif ($currentRuneliteClientHash -ne $migrationTracker.lastRuneliteClientHash) {
+                Write-Output "RuneLite update detected. Copying new RuneLite files..."
+                $migrationTracker.lastRuneliteClientHash = $currentRuneliteClientHash
+                Copy-Files
+            }
+            else
+            {
+                Write-Output "Neither RuneLite or the Steam client have been updated since last run. Cancelling migraion."
+                return
+            }
+        }
+        else 
+        {
+            $migrationTracker = New-Object -TypeName MigrationTracker
+            $migrationTracker.lastRuneliteClientHash = $currentRuneliteClientHash
+            Rename-Item -Path $steamClientPath -NewName $steamClientBackupPath -Force
+            Copy-Files
+        }
+
+        $migrationTracker.lastMigrated = (Get-Date).ToString()
+        $migrationTracker | ConvertTo-Json | Out-File -FilePath $migrationTrackerPath
+        Write-Output "Migration completed successfully."
+    }
+    "Schedule"
+    {
+        if ($Revert)
+        {
+            Unregister-ScheduledTask -TaskName "RuneLite to Steam Migration Job - $Schedule" -Confirm:$false
+        }
+        else
+        {
+            $actionArgs = "-NoProfile -ExecutionPolicy Bypass -File `"$($MyInvocation.MyCommand.Path)`" -RuneLiteInstallPath `"$RuneLiteInstallPath`" -SteamClientInstallPath `"$SteamClientInstallPath`""
+            $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $actionArgs
+            switch ($Schedule)
+            {
+                "Startup" { $trigger = New-ScheduledTaskTrigger -AtStartup }
+                "Logon" { $trigger = New-ScheduledTaskTrigger -AtLogOn }
+            }
+            $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+            $settings = New-ScheduledTaskSettingsSet -MultipleInstances IgnoreNew
+            $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
+            Register-ScheduledTask "RuneLite to Steam Migration Job - $Schedule" -InputObject $task
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Migrate-RuneLiteToSteam
-Powershell script that lets you launch RuneLite through Steam
+A Powershell script that lets you launch RuneLite through Steam
+
+## Getting Started
+All you need to get up and running is to [download a zip of the repo](https://github.com/ewisted/Migrate-RuneLiteToSteam/archive/refs/heads/main.zip), a [RuneLite installation](https://runelite.net/), an [OSRS Steam client installation](https://store.steampowered.com/app/1343370/Old_School_RuneScape/), and Powershell. Once you have those, just follow these steps:
+1. Open up a Powershell window in the directory you downloaded the repo to. The easiest way to do this is to navigate to the folder you downloaded the script to in File Explorer, type `powershell` in the navigation bar, and hit enter.
+2. Enter the following command in the Powershell window. Make sure to replace the placeholder variables with your installation paths.
+  ```
+  .\Migrate-RuneliteToSteam.ps1 -RuneLiteInstallPath "path-to-runelite-files" -SteamClientInstallPath "path-to-steam-client-files"
+  ```
+As long as there were no errors, launching OSRS through Steam should now launch RuneLite.
+
+### Reverting back to the Steam client
+Reverting back is simple. Just run the migration script again with the `-Revert` switch:
+  ```
+  .\Migrate-RuneliteToSteam.ps1 -RuneLiteInstallPath "path-to-runelite-files" -SteamClientInstallPath "path-to-steam-client-files" -Revert
+  ```
+  
+## Run at startup/logon
+Eventually Steam is going to update OSRS and overwrite RuneLite or RuneLite could be updated from its orginal install location. The script takes these cases into account and keeps track of RuneLite's MD5 hash in order to be idempotent. Therefore, it is recommended to schedule to run the script at startup or logon. To do so just follow these steps:
+1. Open up a Powershell window as administrator (type `Powershell` in Windows Search, right click it, and select `Run as administrator`)
+2. Navigate to the directory containing the Powershell script (`cd path-to-directory`)
+3. Rune the script with the `-Schedule` parameter. It accepts either `Startup` or `Logon`.
+  ```
+  .\Migrate-RuneliteToSteam.ps1 -RuneLiteInstallPath "path-to-runelite-files" -SteamClientInstallPath "path-to-steam-client-files" -Schedule "schedule-type"
+  ```
+The `-Schedule` parameter also works with the `-Revert` switch, so using both will remove the scheduled task.
+  ```
+  .\Migrate-RuneliteToSteam.ps1 -RuneLiteInstallPath "path-to-runelite-files" -SteamClientInstallPath "path-to-steam-client-files" -Schedule "schedule-type" -Revert
+  ```
